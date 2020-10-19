b56d5642f3191a9e71e421ce76a5ad1d8e8fb0b2
Nick Mather <nickmather12@gmail.com>
2020-10-20 09:35:24 +1100

    CRUD complete. Moved to nested routes for logbooks - flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,46 @@
 class FlightsController < ApplicationController
   before_action :check_for_login
-  
+
   def index
   end
 
   def new
+    @logbook = Logbook.find params[:logbook_id]
+    @flight = Flight.new
+  end
+
+  def create
+    logbook = Logbook.find params[:logbook_id]
+    @flight = Flight.new flight_params
+    if @flight.save
+      logbook.flights << @flight
+      redirect_to logbook_path(logbook)
+    else
+      render :new
+    end
   end
 
   def edit
+    @logbook = Logbook.find params[:logbook_id]
+    @flight = Flight.find params[:id]
+  end
+
+  def update
+    logbook = Logbook.find params[:logbook_id]
+    flight = Flight.find params[:id]
+    flight.update flight_params
+    redirect_to logbook_path(logbook)
+  end
+
+  def destroy
+    logbook = Logbook.find params[:logbook_id]
+    flight = Flight.find params[:id]
+    flight.destroy
+    redirect_to logbook_path(logbook)
+  end
+
+  private
+  def flight_params
+    params.require(:flight).permit(:date, :aircraft, :registration, :night, :multi, :command, :flight_time, :instrument_time, :notes, :location, :launch_type, :serial, :freefall, :launch_altitude, :distance)
   end
 end
