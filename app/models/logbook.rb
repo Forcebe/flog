093569d5a4cb4a4806fb8bb7c9731c11c22caf3b
Nick Mather <nickmather12@gmail.com>
2020-10-20 10:28:55 +1100

    Added validation to logbooks model for aviation type inputs

--- app/models/logbook.rb
+++ app/models/logbook.rb
@@ -1,4 +1,5 @@
 class Logbook < ApplicationRecord
   has_many :flights
   belongs_to :user, :optional => true
+  validates :aviation_type, inclusion: { in: ['General Aviation', 'Gliding', 'Free Flight', 'Skydiving'], message: "invalid"}
 end
